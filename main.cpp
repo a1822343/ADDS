c3067a1ffc199cf099938ede631db4e92814c5e9
Joel Martin <a1822343@adelaide.edu.au>
2023-04-11 11:54:13 +0930

    cin instead of command line arg

--- main.cpp
+++ main.cpp
@@ -3,14 +3,23 @@
 #include "RecursiveBinarySearch.h"
 
 #include <iostream>
+#include <string>
 
 int main(int argc, char* argv[]){
   QuickSort ascOrder;
   RecursiveBinarySearch result;
   std::vector<int> list;
 
-  for (size_t i = 1; i < argc; i++){
-    list.push_back(atoi(argv[i]));
+  std::string argument;
+
+  // for (size_t i = 1; i < argc; i++){
+  //   list.push_back(atoi(argv[i]));
+  // }
+
+  std::getline(std::cin, argument);
+
+  for (size_t i = 0; i < argument.length(); i += 2){
+    list.push_back(argument[i] - 48);
   }
 
   list = ascOrder.sort(list);
