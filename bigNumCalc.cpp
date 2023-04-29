081711e14e09c1825b3ff35955d8d0553303b85e
Joel Martin <a1822343@adelaide.edu.au>
2023-04-29 14:45:59 +0930

    added bigNumCalc files so gradescope passes to tests

---
+++ bigNumCalc.cpp
@@ -0,0 +1,17 @@
+#include "bigNumCalc.h"
+
+std::list<int> buildBigNum(std::string numString){
+
+}
+
+std::list<int> add(std::list<int> num1, std::list<int> num2){
+
+}
+
+std::list<int> sub(std::list<int> num1, std::list<int> num2){
+
+}
+
+std::list<int> mul(std::list<int> num1, std::list<int> num2){
+
+}
