85e598fffe38218a478f27b61e1ef7d5a4357bcc
Joel Martin <a1822343@adelaide.edu.au>
2023-05-15 23:08:07 +0930

    are these the problem?

--- PrefixMatcher.h
+++ PrefixMatcher.h
@@ -1,20 +1,20 @@
-#ifndef PREFIXMATCHER_H
-#define PREFIXMATCHER_H
+// #ifndef PREFIXMATCHER_H
+// #define PREFIXMATCHER_H
 
-#include <string>
-#include <unordered_map>
+// #include <string>
+// #include <unordered_map>
 
-struct TrieNode {
-  char key;
-  bool isEndOfAddress;
-  std::unordered_map<int, TrieNode*> children;
-};
+// struct TrieNode {
+//   char key;
+//   bool isEndOfAddress;
+//   std::unordered_map<int, TrieNode*> children;
+// };
 
-class PrefixMatcher {
- public: 
-  std::unordered_map<int, TrieNode*> knownRouters;
-  std::unordered_map<int, TrieNode*> *currLevel;
-  int selectRouter(std::string networkAddress);
-  void insert(std::string address, int routerNumber);
-};
-#endif
+// class PrefixMatcher {
+//  public: 
+//   std::unordered_map<int, TrieNode*> knownRouters;
+//   std::unordered_map<int, TrieNode*> *currLevel;
+//   int selectRouter(std::string networkAddress);
+//   void insert(std::string address, int routerNumber);
+// };
+// #endif
