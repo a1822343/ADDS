0ffff2dc9665863a52cd97a80951eb755bbfe7d8
Joel Martin <a1822343@adelaide.edu.au>
2023-05-16 19:56:39 +0930

    prefix, not suffix

--- PrefixMatcher.cpp
+++ PrefixMatcher.cpp
@@ -4,45 +4,37 @@ PrefixMatcher::PrefixMatcher() { currLevel = &knownRouters; }
 
 int PrefixMatcher::selectRouter(std::string networkAddress) {
   int returnRouter = 0;
-  int max = 0;
-  bool inTree = false;
+  //int max = 0;
+  //bool inTree = false;
   for (size_t i = 0; i < networkAddress.length(); i++) {
     for (size_t j = 0; j < currLevel->size(); j++) {
       if (networkAddress[i] == currLevel->at(j)->key) {
+        returnRouter = currLevel->at(j)->routerNumber;
         currLevel = &currLevel->at(j)->children;
-        inTree = true;
-        break;
-      } else {
-        inTree = false;
       }
     }
-    if (!inTree) {
-      return returnRouter;
-    }
   }
-
-  returnRouter = traverse(currLevel, 0, max);
-
+  currLevel = &knownRouters;
   return returnRouter;
 }  // return the router with the longest matching prefix
 
-int PrefixMatcher::traverse(std::vector<TrieNode*> *tree, int currLength, int max){
-  std::vector<TrieNode *> *subTree;
-  int returnRouter = tree->front()->routerNumber;
-  for (size_t i = 0; i < tree->size(); i++) {
-    if (tree->at(i)->isEndOf) {
-      if (currLength > max){
-        max = currLength;
-        returnRouter = tree->at(i)->routerNumber;
-      }
-    } else {
-      subTree = &tree->at(i)->children;
-      currLength++;
-      traverse(subTree, currLength, max);
-    }
-  }
-  return returnRouter;
-}
+// int PrefixMatcher::traverse(std::vector<TrieNode*> *tree, int currLength, int max){
+//   std::vector<TrieNode *> *subTree;
+//   int returnRouter = tree->front()->routerNumber;
+//   for (size_t i = 0; i < tree->size(); i++) {
+//     if (tree->at(i)->isEndOf) {
+//       if (currLength > max){
+//         max = currLength;
+//         returnRouter = tree->at(i)->routerNumber;
+//       }
+//     } else {
+//       subTree = &tree->at(i)->children;
+//       currLength++;
+//       traverse(subTree, currLength, max);
+//     }
+//   }
+//   return returnRouter;
+// }
 
 void PrefixMatcher::insert(std::string address, int routerNumber) {
   // use Autocomplete::insert with an unordered_map
